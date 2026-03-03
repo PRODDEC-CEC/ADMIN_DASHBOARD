e1f45cb55e6c783205af0072ec684d6888a2914b
Proddec-CEC <68556065+PRODDEC-CEC@users.noreply.github.com>
2026-03-03 21:53:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // Replace with your actual Firebase config
+
+
+function test() {
+    if (!import.meta.env.VITE_FIREBASE_API_KEY || !import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || !import.meta.env.VITE_FIREBASE_PROJECT_ID || !import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || !import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || !import.meta.env.VITE_FIREBASE_APP_ID || !import.meta.env.VITE_FIREBASE_MEASUREMENT_ID) {
+        throw new Error("VITE_FIREBASE_API_KEY is not defined");
+    } else {
+        console.log("Successfully imported Firebase config");
+    }
+}
+test();
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
