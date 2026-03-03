7fa6912083597f50f592668c2f6c71e2c92495d0
Proddec-CEC <68556065+PRODDEC-CEC@users.noreply.github.com>
2026-03-03 23:11:56 +0530

    Fixed issue of fetching details from database

--- src/firebase.js
+++ src/firebase.js
@@ -4,23 +4,6 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // Replace with your actual Firebase config
-
-
-function test() {
-    if (!import.meta.env.VITE_FIREBASE_API_KEY || !import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || !import.meta.env.VITE_FIREBASE_PROJECT_ID || !import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || !import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || !import.meta.env.VITE_FIREBASE_APP_ID || !import.meta.env.VITE_FIREBASE_MEASUREMENT_ID) {
-        throw new Error("VITE_FIREBASE_API_KEY is not defined");
-    } else {
-        console.log("Successfully imported Firebase config");
-        console.log(import.meta.env.VITE_FIREBASE_API_KEY);
-        console.log(import.meta.env.VITE_FIREBASE_AUTH_DOMAIN);
-        console.log(import.meta.env.VITE_FIREBASE_PROJECT_ID);
-        console.log(import.meta.env.VITE_FIREBASE_STORAGE_BUCKET);
-        console.log(import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID);
-        console.log(import.meta.env.VITE_FIREBASE_APP_ID);
-        console.log(import.meta.env.VITE_FIREBASE_MEASUREMENT_ID);
-    }
-}
-test();
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -33,6 +16,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
-console.log(db);
